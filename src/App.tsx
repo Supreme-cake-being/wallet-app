9f4d82619537b119f5297b6bce90a5ba0869d89d
Kirill Korchakovskyi <121553762+Supreme-cake-being@users.noreply.github.com>
2026-04-02 11:43:54 +0300

    fixed data,json issue

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ export default function App() {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    fetch("/data.json")
+    fetch(import.meta.env.BASE_URL + "data.json")
       .then((r) => r.json())
       .then((data: Transaction[]) => {
         setTransactions(data);
